f0973753f7a64c28fbbdd213b620b49d43a36e42
Alexander Pegot-Ogier <ap9283@log-3.hpc.nyu.edu>
2025-11-26 15:34:38 -0500

    updated

--- simplecrl/eval_knn.py
+++ simplecrl/eval_knn.py
@@ -73,10 +73,11 @@ def main(args):
     
     # Data transforms (NO augmentation for evaluation!)
     transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                           std=[0.229, 0.224, 0.225])
-    ])
+    transforms.Resize((96,96)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
     
 # ----- Build datasets -----
     # For tuning k (train + val mode)
